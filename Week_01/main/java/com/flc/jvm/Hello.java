0781d3c7b6220752afb7e282d1f04276497e5cb3
fanglc@anchnet.com <fanglc@anchnet.com>
2020-10-18 13:03:24 +0800

    自定义一个 Classloader，加载一个 Hello.xlass 文件，执行 hello 方法，此文件内容是一个 Hello.class 文件所有字节（x=255-x）处理后的文件

---
+++ Week_01/main/java/com/flc/jvm/Hello.java
@@ -0,0 +1,9 @@
+package com.flc.jvm;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello classloader");
+    }
+
+}
---
+++ Week_01/main/java/com/flc/jvm/HelloClassLoader.java
@@ -0,0 +1,39 @@
+package com.flc.jvm;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class HelloClassLoader extends ClassLoader {
+
+    public static void main(String[] args) {
+        try {
+
+            Class<?> clz = new HelloClassLoader().findClass("Hello");
+            Method hello = clz.getDeclaredMethod("hello");
+            hello.setAccessible(true);
+            hello.invoke(clz.newInstance());
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        String filePath = this.getClass().getResource("/Hello.xlass").getPath();
+        File file = new File(filePath);
+        int length = (int) file.length();
+        byte[] bytes = new byte[length];
+        try {
+            new FileInputStream(file).read(bytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return super.findClass(name);
+        }
+        for (int i = 0; i < length; i++) {
+            bytes[i] = (byte) (255 - bytes[i]);
+        }
+        return defineClass(name, bytes, 0, length);
+    }
+}
