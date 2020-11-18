0ed2fc62d181640634230a2b1d1eca06cab23d47
fanglc@anchnet.com <fanglc@anchnet.com>
2020-11-18 18:28:57 +0800

    JDBC配置.

---
+++ Week_05/jdbc/src/test/java/com/flc/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,32 @@
+package com.flc.jdbc;
+
+import com.flc.bean.User;
+import com.flc.jdbc.dao.UserDao;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    void contextLoads() {
+        List<User> users = userDao.findAll();
+        System.out.println(users);
+        userDao.insert(User.builder().id(2).code("12345").name("flc").build());
+        List<User> users1 = userDao.findAll();
+        System.out.println(users1);
+        userDao.update(User.builder().id(1).code("20103259").name("fanglc").build());
+        List<User> users2 = userDao.findAll();
+        System.out.println(users2);
+        userDao.deleteAll();
+        List<User> users3 = userDao.findAll();
+        System.out.println(users3);
+    }
+
+}
