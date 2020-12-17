972ce755b9aabcc2ef80eb70d15995350120ecc7
fanglc@anchnet.com <fanglc@anchnet.com>
2020-12-17 16:36:47 +0800

    第9次课程作业提交-hmily-tcc-dubbo

---
+++ Week_09/hmily-tcc-dubbo/src/main/java/com/flc/hmily/tcc/dubbo/HmilyTccDubboApplication.java
@@ -0,0 +1,17 @@
+package com.flc.hmily.tcc.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @creator fanglc@anch.net
+ * @createdTime 2020/12/17 15:42
+ * @desc
+ */
+@SpringBootApplication
+public class HmilyTccDubboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmilyTccDubboApplication.class, args);
+    }
+}
